603e280f0ba83de8827e10841842cde823d70c70
Terrence Li <terrencegl10@berkeley>
2019-06-01 18:04:59 -0700

    data input with text file implemented

--- Feature Engineering.py
+++ Feature Engineering.py
@@ -18,6 +18,19 @@ import html2text
 import requests
 from goose3 import Goose
 
+article_links = []
+article_labels = []
+
+def extract_urls():
+	url_file = open('url_training_set.txt', 'r')
+	lines = url_file.readlines()
+	for i in range(len(lines)):
+		lines[i] = lines[i][:len(lines[i]) - 1]
+		splitted = lines[i].split(' ') #takes advantage of spaces being illegal in urls
+		article_links.append(splitted[0])
+		article_labels.append(splitted[1])
+
+
 def file_to_string(filename):
 	'''
 	converts filename to a string
@@ -31,7 +44,7 @@ def file_to_string(filename):
 def url_to_goose_and_text(url):
 	'''
 	converts url to string
-	returns goose3 object of article
+	returns goose3 object of article and full text of html page
 
 	Possible problem: the returned string includes extra information from the whole page, rather than just the article itself.
 	'''
@@ -49,26 +62,46 @@ def url_to_goose_and_text(url):
 	goosed_article = gooser.extract(url = url)
 	return (goosed_article, text)
 
-def Tfidf_predictors(url):
+def Tfidf_predictors():
 	'''
-	takes url of an article
-	return tfidf features of the article
+	returns tfidf sparse matrix of the article texts of all urls in article_links
 
 	subtasks:
 	1. identify which words will be the keys of the returned dictionary
 	2. Later on parse the returned dictionary and form ints for as features for the matrix
 	'''
-	parsed = url_to_goose_and_text(url)
-	goose = parsed[0]
-	full_text = parsed[1]
-	cleaned_text = goose.cleaned_text
+	article_texts = []
+	full_texts = [] # in case we need this in the future
+	for url in article_links:
+		parsed = url_to_goose_and_text(url)
+		goose = parsed[0]
+		full_text = parsed[1]
+		cleaned_text = goose.title + ' ' +goose.cleaned_text
+		article_texts.append(cleaned_text)
+		full_texts.append(full_text)
+
 	vectorizer = TfidfVectorizer()
-	#X = vectorizer.fit_transform([cleaned_text])
-	X = vectorizer.fit_transform(['and and and hello penis hello', 'hello hello and and','and and'])
+	tfidf_sparse_matrix = vectorizer.fit_transform(article_texts)
+	#tfidf_sparse_matrix = vectorizer.fit_transform(['and and and hello penis hello', 'hello hello and and','and and'])
+	#print(article_texts)
 	predictors = vectorizer.get_feature_names()
-	return X
+	print(len(vectorizer.vocabulary_))
+	print(len(predictors))
+	print(predictors)
+	print('=--------')
+	print(vectorizer.vocabulary_)
+	return tfidf_sparse_matrix
+
+
+
 def main():
-	print(Tfidf_predictors('https://www.cnn.com/business/live-news/stock-market-news-today-053119/h_44e0ad8e0780f08dd887f354c74ecb32'))
+	extract_urls()
+	print(Tfidf_predictors())
+
+
+
+
+
 if __name__ == "__main__": 
 	main()
 
