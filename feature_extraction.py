a8ee193fc61dffa32309fe05e237497c43b1228b
Terrence Li <terrencegl10@berkeley>
2019-06-26 12:09:29 -0700

    put in grammar feature

--- feature_extraction.py
+++ feature_extraction.py
@@ -1,15 +1,17 @@
 from newspaper import Article
 from goose3 import Goose
+import language_tool
 
 class ArticleVector:
 	'''
 	class whos purpose is to extract an article/urls vector for feature matrix
 	'''
 	reputable_news_sources = open('reputable_news_sources.txt', 'r').read().split(' ')
+	num_dimensions = 4 # changes as unique features are added
+
 	def __init__(self, url):
-		self.num_dimensions = 3 # changes as unique features are added
 		self.url = url
-		self.vector = [0] * self.num_dimensions 
+		self.vector = [0] * num_dimensions 
 		article = self.extract_article()
 		self.title = article.title
 		self.text = article.cleaned_text
@@ -20,10 +22,20 @@ class ArticleVector:
 		if self.text == '':
 			raise Exception('The text for this article is empty.')
 
+	def check_grammar(self):
+		'''
+		returns the number of grammar mistakes of the article
+		'''
+		
+		checker = language_tool.LanguageTool('en-US')
+		matches = language_tool.check(self.text) # of typos. 
+		return len(matches)
+
 	def extract_article(self):
 		'''
 		returns a goose article object
 		'''
+
 		gooser = Goose()
 		article = gooser.extract(url = self.url)
 		return article
@@ -32,10 +44,15 @@ class ArticleVector:
 		'''
 		returns 1 if url has reputable ending, 0 otherwise
 		'''
-		if '.com' in self.url or '.gov' in self.url or 'org' in self.url:
+		reputable_endings = ['.com', '.gov', '.org']
+		rear_period_index = self.url.rindex('.')
+		ending = self.url[rear_period_index : rear_period_index + 4]
+
+		if ending in reputable_endings:
 			return 1
 		else:
 			return 0
+
 	def today_feature(self):
 		'''
 		returns the number of times "today" appears in the article text
@@ -59,6 +76,7 @@ class ArticleVector:
 		'''
 		calls all the methods created to fill in the articlevector
 		'''
-		self.vector[0] = self.url_ending_feature()
-		self.vector[1] = self.from_reputable_source_feature()
-		self.vector[2] = self.today_feature()
+		self.vector[0] = self.url_ending_feature() # reputable url ending feature 
+		self.vector[1] = self.from_reputable_source_feature() # reputable news source feature 
+		self.vector[2] = self.today_feature() #contains 'today' feature
+		self.vector[3] = self.check_grammar() #number of grammar mistakes feature
