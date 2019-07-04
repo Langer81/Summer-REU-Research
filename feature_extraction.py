18f0578c401a01643025c6bab8c9f17f60107d31
Terrence Li <terrencegl10@gmail.com>
2019-07-04 01:31:04 -0400

    grammar_index added

--- feature_extraction.py
+++ feature_extraction.py
@@ -1,6 +1,6 @@
 from newspaper import Article
 from goose3 import Goose
-import language_tool
+import language_check
 
 class ArticleVector:
 	'''
@@ -11,7 +11,7 @@ class ArticleVector:
 
 	def __init__(self, url):
 		self.url = url
-		self.vector = [0] * num_dimensions 
+		self.vector = [0] * ArticleVector.num_dimensions 
 		article = self.extract_article()
 		self.title = article.title
 		self.text = article.cleaned_text
@@ -22,14 +22,14 @@ class ArticleVector:
 		if self.text == '':
 			raise Exception('The text for this article is empty.')
 
-	def check_grammar(self):
+	def grammar_index(self):
 		'''
-		returns the number of grammar mistakes of the article
+		returns the number of grammar mistakes of the article divided by the length of the article
 		'''
 		
-		checker = language_tool.LanguageTool('en-US')
-		matches = language_tool.check(self.text) # of typos. 
-		return len(matches)
+		checker = language_check.LanguageTool('en-US')
+		matches = checker.check(self.text) # of typos. 
+		return len(matches) / len(self.text)
 
 	def extract_article(self):
 		'''
@@ -67,7 +67,7 @@ class ArticleVector:
 		'''
 		returns 1 if urls has reputable source in it
 		'''
-		for source in reputable_news_sources:
+		for source in ArticleVector.reputable_news_sources:
 			if source in self.url:
 				return 1
 		return 0
@@ -79,4 +79,4 @@ class ArticleVector:
 		self.vector[0] = self.url_ending_feature() # reputable url ending feature 
 		self.vector[1] = self.from_reputable_source_feature() # reputable news source feature 
 		self.vector[2] = self.today_feature() #contains 'today' feature
-		self.vector[3] = self.check_grammar() #number of grammar mistakes feature
+		self.vector[3] = self.grammar_index() #number of grammar mistakes feature
